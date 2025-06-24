927137caec29fc13aac447ca6a8c82fa7e4137f7
Muhammad Rohan Ghalib <83542347+RohanGhalib@users.noreply.github.com>
2025-06-24 17:18:34 +0500

    Update
    Addded Link instead of ahref tags, added footer on all pages, optimized the code of atricle viewing page for speed and efficiency

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+ return(
+   <footer className="footer mt-5">
+    <div className="container">
+      <div className="row mt-3 ">
+        <div className="col-lg-4 mt-5">
+          <img src="/logo.svg" height="200" alt="" />
+        </div>
+                <div className="col-lg-4"></div>
+
+        <div className="col-lg-4 mt-5">
+
+          <p className="text-end urdutext fs-2">
+ہمیں جان دینی ہے ایک دن وہ کسی طرح وہ کہیں سہی۔
+<br />ہمیں آپ کھینچے دار پر جو نہیں کوئی تو ہمیں سہی۔
+            <br />
+سر طور ہو سر حشر ہو ہمیں انتظار قبول ہے۔
+<br />وہ کبھی ملیں وہ کہیں ملیں وہ کبھی سہی وہ کہیں سہی۔
+
+          </p>
+        </div>
+      </div>
+    </div>
+   </footer>
+ )
+}
