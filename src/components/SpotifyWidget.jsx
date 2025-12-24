e00f674efc757741604a61583bd566c325d422d4
muhammadrohanghalib <muhammadrohanghalib@gmail.com>
2025-12-24 09:50:02 +0000

    change refresh rate

--- src/components/SpotifyWidget.jsx
+++ src/components/SpotifyWidget.jsx
@@ -37,7 +37,7 @@ const EqualizerBar = ({ delay, isPlaying }) => {
 export default function SpotifyWidget() {
   // --- API HOOKS ---
   const { data: nowPlayingData } = useSWR('/api/now-playing', fetcher, { 
-    refreshInterval: 5000 
+    refreshInterval: 100 
   });
   const { data: topTracksData } = useSWR('/api/top-tracks', fetcher);
 
