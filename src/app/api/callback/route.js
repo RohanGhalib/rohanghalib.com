4e47de9978eda82363ba67af43caab9f8df7fb60
muhammadrohanghalib <muhammadrohanghalib@gmail.com>
2025-12-24 07:17:16 +0000

    feat: Hardcode redirect URI for debugging

--- src/app/api/callback/route.js
+++ src/app/api/callback/route.js
@@ -1,4 +1,3 @@
-
 import { NextResponse } from 'next/server';
 import querystring from 'querystring';
 
@@ -12,7 +11,7 @@ export async function GET(req) {
 
   const client_id = process.env.SPOTIFY_CLIENT_ID;
   const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
-  const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+  const redirect_uri = "https://rohanghalib.com/api/callback";
 
   const response = await fetch('https://accounts.spotify.com/api/token', {
     method: 'POST',
