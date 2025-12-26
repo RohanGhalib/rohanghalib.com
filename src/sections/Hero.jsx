bca04ac3b9c2ac039ced0f31725dfecf570ac568
Muhammad Rohan Ghalib <83542347+RohanGhalib@users.noreply.github.com>
2025-12-26 11:54:24 +0500

    Add tech stack badges using ReactMarkdown in Hero
    Introduced ReactMarkdown to render a set of technology and tool badges in the Hero section, visually showcasing the developer's tech stack and tools. Removed commented-out Typewriter code for cleaner markup.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,7 +4,7 @@ import { Typewriter } from 'react-simple-typewriter';
 import ReactDOM from 'react-dom';
 import SpotifyWidget from '@/components/SpotifyWidget';
 import { useTheme } from "next-themes";
-
+import ReactMarkdown from "react-markdown";
 export default function Hero() {
   ReactDOM.preload("/profile.jpg", { as: "image" });
   const { theme } = useTheme();
@@ -17,17 +17,6 @@ export default function Hero() {
     <img className="profilepicture" src="/profile.jpg" alt="" />
     <h1 className="mt-4">Muhammad Rohan Ghalib</h1>
 
-  
-
-{/* <p className="mb-4"><i><Typewriter
-                words={['Skilled Tech enthusiast', 'CS Student.', 'Open Source Contributor', 'Member of Islami Jamiat Talba', 'President Social Society @ KIPS College', 'Vector Artist who believes in simple art.', 'Pakistani Citizen .', 'Software Engineer', 'Noob Writer', 'Club Leader @ HackClub', 'Founder @ TeenVerse', 'Co Founder @ IntroTaps', 'Music Afficionado', 'Hobbbyist in Blending Urdu into modern Tech.', 'Poetry Lover', 'President School Units @ Bazm-e-Paigham Bahawalpur', 'Leader @ CodeForPakistan Bahawalpur Chapter', 'A Youngster who is still trying to understand his identity.', 'Captures everyone, Captured by none.', 'Ex Arqamite.', 'Ex Educatorian.', 'Hafiz e Quran', 'Not good at all in sports.', 'Unsure about the future, Just going with the flow', 'Loves Photography But cant do it.', 'Cinematography is amazing.' ] }
-                loop={true} // Set to true to loop indefinitely
-                cursor
-                cursorStyle='🙂'
-                typeSpeed={20}
-                deleteSpeed={5}
-                delaySpeed={700}
-              /></i></p>*/}
     <a href="https://instagram.com/rohanghalib" className="socialbutton"><i className="bi bi-instagram"></i></a>
     <a href="https://github.com/rohanghalib" className="ms-2 mt-2 socialbutton"><i className="bi bi-github"></i></a>
     <a href="https://facebook.com/rohanghalib" className="ms-2 mt-2 socialbutton"><i className="bi bi-facebook"></i></a>
@@ -39,7 +28,30 @@ export default function Hero() {
         <Link href="/projects" className="socialbutton">My Projects <i className="bi bi-arrow-up-right-circle-fill"></i></Link>
 <br /><br />
     <Link href="/articles" className="socialbutton mt-5">Read My Articles <i className="bi bi-arrow-up-right-circle-fill"></i></Link>
+    <br /> <br /><br />
+    <ReactMarkdown>
 
+![Next.js](https://img.shields.io/badge/Next.js-black?style=flat\&logo=next.js\&logoColor=white)
+![Vite](https://img.shields.io/badge/Vite-646CFF?style=flat\&logo=vite\&logoColor=white)
+![Laravel](https://img.shields.io/badge/Laravel-%23FF2D20.svg?style=flat\&logo=laravel\&logoColor=white)
+![React](https://img.shields.io/badge/React-%23007ACC.svg?style=flat\&logo=react\&logoColor=white)
+![Firebase](https://img.shields.io/badge/Firebase-%23039BE5.svg?style=flat\&logo=firebase)
+![MySQL](https://img.shields.io/badge/MySQL-%2300f.svg?style=flat\&logo=mysql\&logoColor=white)
+
+
+![GitHub Actions](https://img.shields.io/badge/GitHub%20Automations-2088FF?style=flat\&logo=githubactions\&logoColor=white)
+![Cloudflare](https://img.shields.io/badge/Cloudflare-F38020?style=flat\&logo=cloudflare\&logoColor=white)
+![Linux](https://img.shields.io/badge/Linux-FCC624?style=flat\&logo=linux\&logoColor=black)
+![Windows](https://img.shields.io/badge/Windows-0078D6?style=flat\&logo=windows\&logoColor=white)
+
+
+![Figma](https://img.shields.io/badge/Figma-%23F24E1E.svg?style=flat\&logo=figma\&logoColor=white)
+![Photoshop](https://img.shields.io/badge/Photoshop-31A8FF?style=flat\&logo=adobephotoshop\&logoColor=white)
+![Illustrator](https://img.shields.io/badge/Illustrator-FF9A00?style=flat\&logo=adobeillustrator\&logoColor=white)
+
+
+![Python](https://img.shields.io/badge/Python-3776AB?style=flat\&logo=python\&logoColor=white)
+![Raspberry Pi 5](https://img.shields.io/badge/Raspberry%20Pi-CC0000?style=flat\&logo=raspberrypi\&logoColor=white)    </ReactMarkdown>
     </div>
     <div className="col-lg-6 mt-3">
     <SpotifyWidget isDark={theme === 'dark'} />      <p className="mt-5 text-end">
